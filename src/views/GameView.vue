ee919ff097bb90bc3e261876adc22728fcb94740
Pan-Pan-Pan <1425650574@qq.com>
2024-09-07 17:20:17 +0800

    Update GameView.vue

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="game-wrapper">
-    <iframe src="http://8.130.103.241/game/index.html" width="100%" height="80%"></iframe>
+    <iframe src="http://8.130.103.241/game/index.html" width="1000px" height="630px"></iframe>
   </div>
 </template>
 
@@ -9,12 +9,15 @@ div.game-wrapper {
   width: 100%;
   height: calc(100% - 60px);
   position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-iframe {
+/* iframe {
   border: none; 
   scale: 0.8; 
   position: absolute;
-  /* top: -60px; */
-}
+  top: -60px;
+} */
 </style>
